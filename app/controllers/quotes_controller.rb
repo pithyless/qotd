5a3fe0e22e957ee943a05f47a95d8566a61b61ee
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2012-09-16 16:16:47 +0200

    Add QuotesController

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,7 @@
+class QuotesController < Rulers::Controller
+
+  def index
+    "Did you try turning it off and on again?"
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,9 @@
 require 'rulers'
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require "quotes_controller"
+
 module Qotd
   class Application < Rulers::Application
 
